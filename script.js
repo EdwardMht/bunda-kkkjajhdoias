1ccad824f6b07b8adc5b276d244f5e21477bc8a5
BoresCuia <101402962+EdwardMht@users.noreply.github.com>
2024-06-19 08:41:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    enunciado: "em relação as queimadas no Brasil, o que vocẽ acha que deveria ser feito?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "as pessoas deveriam parar com as queimadas.",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "o governo deveria tomar mais providẽncias para acabar com as queimadas",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "quais providẽncias o Governo e as pessoas podem tomar, para que esse problema seja resolvido?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Leis podem ser criadas e os cidadões podem ajudar a monitorar e descibrir quem começa as queimadas",
                 afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "o governo pode oferecer benefícios, para quem ajudar a combater e ajudar contra as queimadas.",
                 afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "vocẽ está na sua casa e decide sair. Ao sair se depara com um vizinho seu jogando lixo na rua, o que vocẽ faz?",
         alternativas: [
             {
                 texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
